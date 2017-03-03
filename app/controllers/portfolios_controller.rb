8882eb986ddabb82250dd37ee36cc4a2c3693ad4
Pontus Rossing <pontus@rossing.se>
2017-03-03 08:46:38 +0100

    Added link functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
@@ -25,7 +29,7 @@ class PortfoliosController < ApplicationController
 
   def update
   	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-  	
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Item was successfully updated.' }
