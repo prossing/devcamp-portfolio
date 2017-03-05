9786fa14c838f05f3e09b6985350a7038406afce
Pontus Rossing <pontus@rossing.se>
2017-03-05 16:38:58 +0100

    Implemented dynamic account links

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,7 +21,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -33,7 +33,7 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-  	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  	@portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -51,6 +51,16 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name]
+                                      )
   end
 
 end
