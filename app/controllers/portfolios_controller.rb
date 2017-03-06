886227d7c5dbcc08abdadcf217cbc560964d9d6b
Pontus Rossing <pontus@rossing.se>
2017-03-06 13:31:14 +0100

    Small patches

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
-	layout "portfolio"
+	before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout "portfolio"
 
   def index
 		@portfolio_items = Portfolio.all
@@ -10,7 +11,6 @@ class PortfoliosController < ApplicationController
   end
 
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def new
@@ -19,7 +19,6 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def create
@@ -35,8 +34,6 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-  	@portfolio_item = Portfolio.new(portfolio_params)
-
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Item was successfully updated.' }
@@ -47,8 +44,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-  	@portfolio_item = Portfolio.find(params[:id])
-
   	@portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
@@ -63,6 +58,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
